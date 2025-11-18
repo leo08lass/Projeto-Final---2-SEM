064bcc3145567664f3b6e76fc6f5258fa487f690
Leonardo Aleixo <leonardo.aleixo@aluno.ifsp.edu.br>
2025-11-18 12:08:31 -0300

    Revert "cad/login bonitos"
    This reverts commit a97b967ba4c81ebd1ee226b9b1bbe826a784dbda.

--- app/src/routes/Routes.js
+++ app/src/routes/Routes.js
@@ -7,7 +7,6 @@ const controllerU = require("../controllers/ControllerUsuarios")
 router.get("/", controllers.paginaInicial);
 router.get("/inscrever", (req, res) => res.render("cadastro_cliente"));
 router.get("/inscrever_funcionario", (req, res) => res.render("cadastro_funcionario"));
-router.get("/login", (req, res) => res.render("login_cliente"));
 router.get("/login_cliente", (req, res) => res.render("login_cliente"));
 router.get("/login_funcionario", (req, res) => res.render("login_funcionario"));
 router.get("/trabalhe_conosco", (req, res) => res.render("funcionario"));
